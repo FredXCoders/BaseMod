3ac69c33f14ca7b10e5f3cf479f0cd43395b3523
Elliott Sperlazza <design@sperlazzasaurus.com>
2016-02-23 22:09:43 -0500

    added Dockerfile to build a container for the mod build environment and script to run the build in the container and extract the mod jar build artifact

---
+++ dockerBuildMod.sh
@@ -0,0 +1,10 @@
+# Copy BaseMod.java into ${PWD}/artifacts, then run this script
+
+docker run -i --rm -v ${PWD}/artifacts:/artifacts fredxcoders/mod-build-env:latest /bin/bash << COMMANDS
+cd /home
+cp /artifacts/BaseMod.java src/main/java/com/fredxcoders/mod/BaseMod.java
+./gradlew build
+cp build/libs/modid-1.0.jar /artifacts/fredxcodersBaseMod.jar
+#echo Changing owner from \$(id -u):\$(id -g) to $(id -u):$(id -u)
+#chown -R $(id -u):$(id -u) /artifacts
+COMMANDS
